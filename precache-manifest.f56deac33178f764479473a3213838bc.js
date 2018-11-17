62094468c1be7d3c2f66db7dc8527495863d1e68
Reeve Johnson <reevewjohnson@gmail.com>
2018-11-17 13:42:46 -0600

    Updates

---
+++ precache-manifest.f56deac33178f764479473a3213838bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b3c7c293c045bf2b88c",
+    "url": "/beyonce-click/static/js/runtime~main.8b3c7c29.js"
+  },
+  {
+    "revision": "ec34cc21a00c74cd937c",
+    "url": "/beyonce-click/static/js/main.ec34cc21.chunk.js"
+  },
+  {
+    "revision": "34c119e77c0263251c51",
+    "url": "/beyonce-click/static/js/1.34c119e7.chunk.js"
+  },
+  {
+    "revision": "ec34cc21a00c74cd937c",
+    "url": "/beyonce-click/static/css/main.d01ea892.chunk.css"
+  },
+  {
+    "revision": "34c119e77c0263251c51",
+    "url": "/beyonce-click/static/css/1.e671be52.chunk.css"
+  },
+  {
+    "revision": "05e3377b532323fe2da6853e0721fd6c",
+    "url": "/beyonce-click/index.html"
+  }
+];
